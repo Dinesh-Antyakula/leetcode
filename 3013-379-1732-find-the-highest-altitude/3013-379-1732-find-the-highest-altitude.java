aba7929231815ef2153eb781935de3c1f0ea32e8
Dinesh-Antyakula <159713750+Dinesh-Antyakula@users.noreply.github.com>
2025-02-19 12:41:02 +0530

    Time: 0 ms, Memory: 41.3 MB - LeetHub

---
+++ 3013-379-1732-find-the-highest-altitude/3013-379-1732-find-the-highest-altitude.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int[] altitudes=new int[gain.length+1];
+        int max=-999;
+        altitudes[0]=0;
+        for(int i=0;i<gain.length;i++){
+            altitudes[i+1]=altitudes[i]+gain[i];
+            if(altitudes[i]>max){
+                max=altitudes[i];
+            }
+            
+        }
+        if(altitudes[gain.length]>max){
+            max=altitudes[gain.length];
+        }
+        return max;
+        
+    }
+}
