dbc03ebf073c8bcbba38d9b4144ddd2dd60ef796
Dinesh-Antyakula <159713750+Dinesh-Antyakula@users.noreply.github.com>
2025-02-26 17:08:53 +0530

    Time: 0 ms, Memory: 43.4 MB - LeetHub

---
+++ 8864-1074-81-search-in-rotated-sorted-array-ii/8864-1074-81-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,80 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int a=rsasearch(nums,target);
+        if(a!=-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+    public int binarySearch(int[]arr,int target,int start,int end){
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>target){
+                end=mid-1;
+            }
+            else if(arr[mid]<target){
+                start=mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+    public int rsasearch(int[] arr,int target){
+        int pivot=findPivotWithDuplicates(arr);
+        if(pivot==-1){
+            return binarySearch(arr,target,0,arr.length-1);
+        }
+        if(arr[pivot]==target){
+            return pivot;
+        }
+        if(target>=arr[0]){
+            return binarySearch(arr,target,0,pivot-1);
+        }
+        return binarySearch(arr,target,pivot+1,arr.length-1);
+    }
+
+     
+   public  int findPivotWithDuplicates(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+
+            // if elements at middle, start, end are equal then just skip the duplicates
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                // skip the duplicates
+                // NOTE: what if these elements at start and end were the pivot??
+                // check if start is pivot
+                if (start < end && arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+
+                // check whether end is pivot
+                if (end > start && arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted, so pivot should be in right
+            else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
